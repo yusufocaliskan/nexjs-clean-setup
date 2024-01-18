a87255cce704efc66eb198843a5562e006fc887e
Yusuf Caliskan <yusufocaliskan@gmail.com>
2024-01-18 18:56:20 +0300

    Listing loging history

--- src/components/ScreenCommons/Card/index.js
+++ src/components/ScreenCommons/Card/index.js
@@ -1,33 +1,43 @@
 import styled from '@emotion/styled';
 
 const Card = (props) => {
-  const {children, id, width, height, bg, display, style} = props;
+  const {children, id, className, bg, style, ...rest} = props;
 
   const testDataAttr = process.env.NODE_ENV === 'test' ? {'data-testid': `${id}`} : id;
 
   const CardWrapper = styled.div`
-    width: ${(props) => props.width?.base};
-    display: ${(props) => props.display?.base};
-    height: ${(props) => props.height?.base};
-    background: ${(props) => props.bg?.base};
+    width: ${(props) => props.width?.base || props.width || '100%'};
+    display: ${(props) => props.display?.base || props.display};
+    height: ${(props) => props.height?.base || props.height};
+    background: ${(props) => props.bg?.base || props.bg};
+    justify-content: ${(props) => props.justifyContent?.base || props.justifyContent};
+    align-items: ${(props) => props.alignItems?.base || props.alignItems};
+    gap: ${(props) => props.gap?.base || props.gap};
 
     @media (min-width: 768px) {
       width: ${(props) => props.width?.md};
       display: ${(props) => props.display?.md};
       background: ${(props) => props.bg?.md};
       height: ${(props) => props.height?.md};
+      justify-content: ${(props) => props.justifyContent?.md};
+      align-items: ${(props) => props.alignItems?.md};
+      gap: ${(props) => props.gap?.md};
     }
     @media (min-width: 1024px) {
       width: ${(props) => props.width?.lg};
       display: ${(props) => props.display?.lg};
       height: ${(props) => props.height?.lg};
       background: ${(props) => props.bg?.lg};
+      justify-content: ${(props) => props.justifyContent?.lg};
+      align-items: ${(props) => props.alignItems?.lg};
+      gap: ${(props) => props.gap?.lg};
     }
   `;
+
   return (
     <>
       <style></style>
-      <CardWrapper display={display} bg={bg} width={width} height={height} {...testDataAttr} id={id} style={style}>
+      <CardWrapper className={className} {...testDataAttr} {...rest} id={id} style={style}>
         {children}
       </CardWrapper>
     </>
--- src/screens/account/profile/login-history/index.js
+++ src/screens/account/profile/login-history/index.js
@@ -1,4 +1,6 @@
+import {Card, LoadingGif, Spacer, Text} from '@/components';
 import {userApi} from '@/services/user';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from '@mui/material';
 import {useEffect} from 'react';
 
 const AccountLoginHistory = () => {
@@ -9,7 +11,44 @@ const AccountLoginHistory = () => {
   useEffect(() => {
     console.log(loginHistoryResponse);
   }, [loginHistoryResponse]);
-  return <div></div>;
+  return (
+    <Card>
+      <Spacer height={{lg: '40px', base: '50px', md: '30px'}} />
+      <Card display="flex" alignItems="center" gap="1rem">
+        <Text>Login History</Text>
+        {loginHistoryResponse.isLoading && <LoadingGif isPuff />}
+      </Card>
+
+      {!loginHistoryResponse.isLoading && (
+        <Card className="table-wrapper">
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Divice</TableCell>
+                  <TableCell>Location</TableCell>
+                  <TableCell>IP Address</TableCell>
+                  <TableCell>Time</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {loginHistoryResponse.data?.Data.map((item, index) => {
+                  return (
+                    <TableRow key={index}>
+                      <TableCell>{item.d}</TableCell>
+                      <TableCell>{item.l}</TableCell>
+                      <TableCell>{item.i}</TableCell>
+                      <TableCell>{item.t}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Card>
+      )}
+    </Card>
+  );
 };
 
 export default AccountLoginHistory;
